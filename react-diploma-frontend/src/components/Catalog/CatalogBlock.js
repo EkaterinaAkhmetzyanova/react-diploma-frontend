1c990a1630aeab4d4937602716c775b33fa5d581
unknown <a.ekaterina323@gmail.com>
2021-12-12 04:06:23 +0300

    Revert "first commit"
    This reverts commit 3bbdc6d2bb50d8b8aceea618d34be654bd618db2.

--- react-diploma-frontend/src/components/Catalog/CatalogBlock.js
+++
@@ -1,8 +0,0 @@
-export default function CatalogBlock(props) {
-  return (
-    <section className="catalog">
-      <h2 className="text-center">Каталог</h2>
-      {props.children}
-    </section>
-  );
-}
--- react-diploma-frontend/src/pages/ProductPage.js
+++
@@ -1,5 +0,0 @@
-import ProductCard from "../components/Products/ProductCard";
-
-export default function ProductPage({ match }) {
-  return <ProductCard id={Number(match.params.id)} />;
-}
